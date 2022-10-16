9f9a360af71e2cbb87de7a9382101cbe62b15123
gyoungseok <azd7777work@gmail.com>
2022-10-17 01:52:21 +0900

    Update SQL_basic

--- SQL_practice/SQL_basic.sql
+++ SQL_practice/SQL_basic.sql
@@ -6,3 +6,20 @@ SELECT * FROM Customer
 WHERE CustID = 1;
 
 -- ORDER BY, 오름차순 또는 내림차순으로 정렬
+SELECT * FROM Customer
+ORDER BY Country ASC;
+
+SELECT * FROM Customer
+ORDER BY Country DESC;
+
+-- INSERT INTO 
+INSERT INTO Customer (`companyName`, `contactName`, `address`, `city`, `postalCode`, `country`)
+VALUES ('cardinal', 'Tom', 'Skagen 21', 'Stavanger', '4006', 'Norway');
+
+
+INSERT INTO Customer (companyName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
+
+SELECT * FROM Customer ORDER BY custid DESC LIMIT 3;
+
+
